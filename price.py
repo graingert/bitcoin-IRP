a23f4f16b0cec7f67f9980695be4804bdc7ea879
Thomas Grainger <tagrain@gmail.com>
2013-04-28 15:45:58 +0100

    fix price for rounding up

--- price.py
+++ price.py
@@ -4,10 +4,19 @@ import requests
 
 from math import log10, floor
 
-for market in requests.get("http://api.bitcoincharts.com/v1/markets.json").json():
-    if market["symbol"] == "mtgoxUSD":
-        price = int(round(market["close"], -int(floor(log10(market["close"])))))
 
-with open(sys.argv[1], 'w') as f:
-    print("over \\${price}".format(price=price), end="", file=f)
-    print(r"\footnote{\url{http://bitcoincharts.com/markets/mtgoxUSD.html}}", end="", file=f)
+def one_sig_fig(price):
+    sig = int(round(price, -int(floor(log10(price)))))
+    return (sig, "over" if price-sig > 0 else "just under")
+
+if __name__ == "__main__":
+    for market in requests.get("http://api.bitcoincharts.com/v1/markets.json").json():
+        if market["symbol"] == "mtgoxUSD":
+            price, direction = one_sig_fig(market["close"])
+
+    with open(sys.argv[1], 'w') as f:
+        print("{direction} \\${price}".format(
+            price=price,
+            direction=direction
+        ), end="", file=f)
+        print(r"\footnote{\url{http://bitcoincharts.com/markets/mtgoxUSD.html}}", end="", file=f)
