bf905f5bac12ac7979a83f7e9658f55844ea13ca
Thomas Grainger <tagrain@gmail.com>
2013-05-29 15:53:57 +0100

    change price to support both types of requests

--- price.py
+++ price.py
@@ -1,16 +1,22 @@
 from __future__ import print_function
 import sys
 import requests
+import six
 
 from math import log10, floor
 
+def call_or_get(prop):
+    if six.callable(prop):
+        return prop()
+    else:
+        return prop
 
 def one_sig_fig(price):
     sig = int(round(price, -int(floor(log10(price)))))
     return (sig, "over" if price-sig > 0 else "just under")
 
 if __name__ == "__main__":
-    for market in requests.get("http://api.bitcoincharts.com/v1/markets.json").json():
+    for market in call_or_get(requests.get("http://api.bitcoincharts.com/v1/markets.json").json):
         if market["symbol"] == "mtgoxUSD":
             price, direction = one_sig_fig(market["close"])
 
